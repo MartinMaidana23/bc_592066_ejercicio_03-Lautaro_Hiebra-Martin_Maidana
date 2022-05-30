672f2cf007123a2ecbedbc247d021809bbe71638
Martin Maidana <martinmaidana90@gmail.com>
2022-05-30 19:28:32 -0300

    agrego js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let nombre = prompt('Ingrese su nombre completo')
+
+let edad = prompt('Ingrese su edad')
+
+if (edad <= 30) {
+    console.log('Hola, ¿como andas?')
+} else {
+    console.log(`Bienvenido ${nombre} ¿Como estas?`)
+}
